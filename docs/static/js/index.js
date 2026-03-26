b4b17641507407c686196297109dfd54173eb02a
Kaleab Kinfu <kinfu@jhu.edu>
2026-03-26 11:52:05 -0400

    add video samples

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -41,22 +41,19 @@ $(document).ready(function() {
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+    // Initialize generation carousel with fewer slides
+    bulmaCarousel.attach('#generation-carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      breakpoints: [
+        { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 },
+        { changePoint: 1024, slidesToShow: 2, slidesToScroll: 1 }
+      ]
+    });
 
     /*var player = document.getElementById('interpolation-video');
     player.addEventListener('loadedmetadata', function() {
